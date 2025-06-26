248391ac5872b6ab099562309c2b5c75fd39452e
Dmitriy <klin234@inbox.ru>
2025-06-26 18:18:52 +0300

    Initial commit

---
+++ analytic-service/src/test/java/com/example/analytic_service/AnalyticServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.analytic_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
