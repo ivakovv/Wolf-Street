136b972ae3c1fc8301e5bf9fbd320bc852eaf1de
ivakov <ivakov.andrey.2004@mail.ru>
2025-07-06 15:49:12 +0300

    added kafka and proto

---
+++ matching-engine/src/main/java/com/example/matching_engine/component/KafkaConsumer.java
@@ -0,0 +1,23 @@
+package com.example.matching_engine.component;
+
+import com.aws.protobuf.OrderMessages;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class KafkaConsumer {
+
+
+    @KafkaListener(topics = "orders", groupId = "matching-engine-group")
+    public void consumeOrderCreated(OrderMessages.OrderCreatedEvent message) {
+
+    }
+
+    @KafkaListener(topics = "orders", groupId = "matching-engine-group")
+    public void consumeOrderUpdated(OrderMessages.OrderUpdatedEvent message) {
+
+    }
+
+}
