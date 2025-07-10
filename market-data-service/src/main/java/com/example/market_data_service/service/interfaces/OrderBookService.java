1d7759e983b66cd98c5f779951b81f9c39b2c4c0
Dmitriy <klin234@inbox.ru>
2025-07-10 17:56:54 +0300

    Added spread for order books

--- market-data-service/src/main/java/com/example/market_data_service/service/interfaces/OrderBookService.java
+++ market-data-service/src/main/java/com/example/market_data_service/service/interfaces/OrderBookService.java
@@ -2,9 +2,10 @@ package com.example.market_data_service.service.interfaces;
 
 import com.example.market_data_service.dto.orderbook.OrderBookAggregatedResponse;
 import com.example.market_data_service.dto.orderbook.OrderBookResponse;
+import com.example.market_data_service.dto.orderbook.SpreadResponse;
 
 public interface OrderBookService {
     OrderBookResponse getOrderBook(Long instrumentId, Long limitOrders);
-
     OrderBookAggregatedResponse getAggregatedOrderBook(Long instrumentId, Long limitLevels);
+    SpreadResponse getSpread(Long instrumentId);
 }
