e747cd3e4ab27caf18f0f6ed44b3465320fd2299
Dmitriy <klin234@inbox.ru>
2025-07-13 22:33:38 +0300

    redis config fix

--- market-data-service/src/main/java/com/example/market_data_service/config/RedisConfig.java
+++ market-data-service/src/main/java/com/example/market_data_service/config/RedisConfig.java
@@ -4,15 +4,26 @@ import com.example.market_data_service.dto.ohlc.Ohlc;
 import com.example.market_data_service.dto.orderbook.OrderBookEntry;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(
+            @Value("${spring.redis.host}") String host,
+            @Value("${spring.redis.port}") int port) {
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
+    }
+
     @Bean
     public ObjectMapper redisObjectMapper(){
         ObjectMapper mapper = new ObjectMapper();
