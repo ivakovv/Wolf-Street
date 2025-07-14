9478f463bd0e8b5fdef4a43e7b2cedab609094b9
Dmitriy <klin234@inbox.ru>
2025-07-14 18:03:00 +0300

    user-service fixed

--- user-service/src/main/java/com/example/user_service/config/KafkaConfig.java
+++ user-service/src/main/java/com/example/user_service/config/KafkaConfig.java
@@ -19,13 +19,15 @@ import io.confluent.kafka.serializers.protobuf.KafkaProtobufSerializerConfig;
 public class KafkaConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
+    @Value("${spring.kafka.schema-registry-url}")
+    private String schemaRegistryUrl;
     @Bean
     Map<String, Object> producerConfigs(){
         Map<String, Object> config = new HashMap<>();
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaProtobufSerializer.class);
-        config.put(KafkaProtobufSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8081");
+        config.put(KafkaProtobufSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl);
         config.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, false);
         config.put(ProducerConfig.RETRIES_CONFIG, 3);
         config.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, 30000);
